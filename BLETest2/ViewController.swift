3fb080d689232ec5c6b2de0e1f6b3ef6d332b0b7
Roth <aimvyy44@gmail.com>
2022-04-27 16:57:26 +1000

    init

--- BLETest2/ViewController.swift
+++ BLETest2/ViewController.swift
@@ -6,12 +6,51 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    var centralMagager : CBCentralManager?
+    var peripheral : CBPeripheral?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        centralMagager = CBCentralManager(delegate: self, queue: nil)
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            central.scanForPeripherals(withServices: nil)
+            print("is scanning")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print(peripheral.name)
+        print(peripheral.services)
+//        print(peripheral.)
+        if let name = peripheral.name {
+            if name == "朱若慈的MacBook Pro" {
+                centralMagager?.stopScan()
+                centralMagager?.connect(peripheral)
+                self.peripheral = peripheral
+            }
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        peripheral.discoverServices(nil)
+        peripheral.delegate = self
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        print("didDiscoverServices")
+        if let services = peripheral.services {
+            for service in services {
+                print(service)
+                peripheral.discoverCharacteristics(nil, for: service)
+            }
+        }
     }
 
 
