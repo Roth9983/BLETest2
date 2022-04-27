8d1ad313927a7855a1fae5e657bd1ba28579df9f
朱若慈 <roth_chu@books.com.tw>
2022-04-27 14:54:49 +1000

    Initial Commit

---
+++ BLETest2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLETest2
+//
+//  Created by 朱若慈 on 2022/4/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
